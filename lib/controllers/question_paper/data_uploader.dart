64faede61d343c2cba0368262059abc0daf86190
GitRockie <elromankos@gmail.com>
2022-11-06 15:45:56 +0100

    :open_book Read Data from Assets folder
    :outbox_tray: Uploading assets folder and items. AssetManifest.json saved inside 'manifestContent'.
    :loop: Loop through
    :black_joker: Working on json papers

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,14 +1,34 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class DataUploader extends GetxController {
   @override
   void onReady() {
+    //Place to upload data to Firebase backend. Only get called once
     uploadData();
-    //Upload data to Firebase backend. Only get called once
     super.onReady();
   }
 
-  void uploadData() {
-    print('Data is uploading');
+  Future<void> uploadData() async {
+    //Uploading assets folder and items
+    //AssetManifest.json saved inside 'manifestContent'
+    final manifestContent = await DefaultAssetBundle.of(Get.context!)
+        .loadString('AssetManifest.json');
+    //Decoding process. Reading with Map
+    final Map<String, dynamic> manifestMap = json.decode(manifestContent);
+    //Point the path I want, need to get the keys.
+    //Load json file and print path
+    final papersInAssets = manifestMap.keys
+        .where((path) =>
+            path.startsWith('assets/DB/papers') && path.contains('.json'))
+        .toList();
+    //read the content in a loop
+    for (var paper in papersInAssets) {
+      String stringPaperContent = await rootBundle.loadString(paper);
+      print(stringPaperContent);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data_uploader_screen.dart';
 import 'package:get/get.dart';
@@ -7,6 +7,7 @@ Future<void> main() async {
   //WidgetsFlutterBinding.ensureInitialized();
   //await Firebase.initializeApp();
   runApp(GetMaterialApp(
+    debugShowCheckedModeBanner: false,
     home: DataUploaderScreen(),
   ));
 }
