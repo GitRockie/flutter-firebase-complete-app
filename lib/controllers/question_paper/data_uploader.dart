4e1f137684b865665d9651745f6a1fc9fe329f15
GitRockie <elromankos@gmail.com>
2022-11-07 12:36:34 +0100

    :face_in_clouds: Firestore Database
    :seedling: Dependency set
    :bouquet: Creating collection referecies
    :sunflower: Access and Setting properties to To upload multiple data to the backend

--- lib/controllers/question_paper/data_uploader.dart
+++ lib/controllers/question_paper/data_uploader.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_application_1/firebase_ref/references.dart';
 import 'package:flutter_application_1/models/question_paper_model.dart';
 import 'package:get/get.dart';
 
@@ -14,6 +16,9 @@ class DataUploader extends GetxController {
   }
 
   Future<void> uploadData() async {
+    //Place to get Firestore instance to send the data to the backend
+    final fireStore = FirebaseFirestore.instance;
+
     //Uploading assets folder and items
     //AssetManifest.json saved inside 'manifestContent'
     final manifestContent = await DefaultAssetBundle.of(Get.context!)
@@ -35,7 +40,21 @@ class DataUploader extends GetxController {
           .add(QuestionPaperModel.fromJson(json.decode(stringPaperContent)));
     }
     //print('Items number ${questionPapers.length}');
-    //print('Items number ${questionPapers[0].id}');
+    //print('Items description: ${questionPapers[0].description}');
     //print('Items number ${questionPapers[2].description}');
+
+    //To upload multiple data to the backend
+    var batch = fireStore.batch();
+
+    for (var paper in questionPapers) {
+      batch.set(questionPaperRF.doc(paper.id), {
+        'title': paper.title,
+        'img_url': paper.imageUrl,
+        'Description': paper.description,
+        'time_seconds': paper.timeSeconds,
+        'questions_count':
+            paper.questions == null ? 0 : paper.questions!.length,
+      });
+    }
   }
 }
---
+++ lib/firebase_ref/references.dart
@@ -0,0 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//Creating collection referecies
+final fireStore = FirebaseFirestore.instance;
+final questionPaperRF = fireStore.collection('questionPaper');
