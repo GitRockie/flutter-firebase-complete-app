8d10a21da80dd240f4b2b2f2be3c0d0ec35a381d
Roman Kostyuchenko <76057139+GitRockie@users.noreply.github.com>
2022-11-10 12:16:55 +0100

    :star2: Introduction Screen Layout
    :flags: Icon, Text add.
    :circus_tent:  CrcleButton Widget create.
    :christmas_tree: Adding properties. Constructor update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data_uploader_screen.dart';
+import 'package:flutter_application_1/screens/introduction/introduction.dart';
 import 'package:flutter_application_1/screens/splash/splash_screen.dart';
 import 'package:get/get.dart';
 
@@ -15,7 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: SplashScreen());
+        debugShowCheckedModeBanner: false,
+        home: AppIntroductionScren()
+    );
   }
 }
 
---
+++ lib/screens/introduction/introduction.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/widgets/app_circle_button.dart';
+import 'package:get/get.dart';
+
+class AppIntroductionScren extends StatelessWidget {
+  const AppIntroductionScren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        child: Padding(
+          padding:  EdgeInsets.symmetric(horizontal: Get.width * 0.2),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+              const Icon(Icons.star, size: 65, color: Colors.amber,),
+              const SizedBox(height: 40,),
+              const Text('Play & study with Classmoor App right now! Learn whatever you want, find your teachers, or create your own classes. Share yor knowledge and results with friends!'),
+              const SizedBox(height: 40,),
+              AppCircleButton(
+                onTap: () => null,
+                child: const Icon(Icons.arrow_forward_ios, size: 35,),
+                
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
