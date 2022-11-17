94a1ead375f5285fc3625b79cb549d80323d716e
GitRockie <elromankos@gmail.com>
2022-11-17 14:55:55 +0100

    :bamboo: Scaffold Wrap. Issue solved
    :giraffe: Header text update  custom_text_style
    :octocat: AppIcons update

--- lib/configs/themes/custom_text_style.dart
+++ lib/configs/themes/custom_text_style.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/configs/themes/app_colors.dart';
 
 import 'ui_parameters.dart';
 
 TextStyle cardTitles(context) => TextStyle(
-  color: UIParameters.isDarkMode()
-  ? Theme.of(context).textTheme.bodyText1!.color
-  : Theme.of(context).primaryColor,
-  fontSize: 18,
-  fontWeight: FontWeight.bold
-                        
-  );
+    color: UIParameters.isDarkMode()
+        ? Theme.of(context).textTheme.bodyText1!.color
+        : Theme.of(context).primaryColor,
+    fontSize: 18,
+    fontWeight: FontWeight.bold);
 
-const detailText = TextStyle(
-  fontSize: 12
-);
+const detailText = TextStyle(fontSize: 12);
+const headerText = TextStyle(
+    fontSize: 22, fontWeight: FontWeight.w700, color: onSurfaceTextColor);
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -16,74 +16,75 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     //savin Controller instance
     QuestionPaperController _questionPaperController = Get.find();
-    return Container(
-        //decoration: BoxDecoration(gradient: mainGradient()),
-        color: Colors.indigo,
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(mobileScreenPadding),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Icon(
-                      AppIcons.menuLeft,
-                      color: Colors.red,
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(mobileScreenPadding),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Icon(
-                                Icons.accessibility_new_outlined,
-                                color: Colors.red,
-                              ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              Text(
-                                'Hello! I am The BIG TITLE!',
-                                style: detailText.copyWith(
-                                    color: onSurfaceTextColor),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    )
-                  ],
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(gradient: mainGradient()),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(mobileScreenPadding),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Icon(
+                        AppIcons.menuLeft,
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              AppIcons.peace,
+                            ),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'Hello, Moormate!',
+                              style: detailText.copyWith(
+                                  color: onSurfaceTextColor),
+                            )
+                          ],
+                        ),
+                      ),
+                      const Text(
+                        'What do you want to study today?',
+                        style: headerText,
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child: ContentArea(
-                  addPadding: false,
-                  child: Obx(
-                    () => ListView.separated(
-                      padding: UIParameters.mobileScreenPadding,
-                      shrinkWrap: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return QuestionCard(
-                          model: _questionPaperController.allPapers[index],
-                        );
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const SizedBox(
-                          height: 20,
-                        );
-                      },
-                      itemCount: _questionPaperController.allPapers.length,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: ContentArea(
+                      addPadding: false,
+                      child: Obx(
+                        () => ListView.separated(
+                          padding: UIParameters.mobileScreenPadding,
+                          shrinkWrap: true,
+                          itemBuilder: (BuildContext context, int index) {
+                            return QuestionCard(
+                              model: _questionPaperController.allPapers[index],
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return const SizedBox(
+                              height: 20,
+                            );
+                          },
+                          itemCount: _questionPaperController.allPapers.length,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ));
+              ],
+            ),
+          )),
+    );
   }
 }
