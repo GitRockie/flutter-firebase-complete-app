b66594cc75c2447c9ca64d99c0306992dd78757b
GitRockie <elromankos@gmail.com>
2022-11-20 13:19:14 +0100

    :juggling_person: AuthController update
    Updating pubspec plugins.
    Logger class create to log information which is helpful for debugging.
    :lotus_position: Getting SignIn object then the method, we get autentication from Google.
    :people_hugging: Credentials get sending to Firebase.
    :magic_wand: Saving User information if loggeed.
    :nesting_dolls: Firestore create User collection. Build - Authentication.

--- lib/controllers/question_paper/auth_controller.dart
+++ lib/controllers/question_paper/auth_controller.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
@@ -8,10 +9,25 @@ class AuthController extends GetxController {
     super.onReady();
   }
 
+  //Later initialization for _auth RX type since we're using Get X
+  late FirebaseAuth _auth;
+  final _user = Rxn<User>();
+  //Creating User Stream, if logged or not
+  late Stream<User?> _authStateChanges;
+
   void initAuth() async {
     await Future.delayed(const Duration(seconds: 2));
+    _auth = FirebaseAuth.instance;
+    _authStateChanges = _auth.authStateChanges();
+    //listening event to the User changes passing User Object
+    _authStateChanges.listen((User? user) {
+      //saving the _user to the user value
+      _user.value = user;
+    });
     navigateToIntroduction();
   }
+
+  //Google SignIn method
 
   void navigateToIntroduction() {
     Get.offAllNamed('/introduction');
