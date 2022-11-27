ff56d29b9fa9264c76fa55babf596f5892a08085
GitRockie <elromankos@gmail.com>
2022-11-27 21:07:16 +0100

    Question ScreenLoad Data
    TODO List:
    Select answers/Go to theNext page/Counter add/Menu/Custom paint

---
+++ lib/screens/question/question_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class QuestionScreen extends StatelessWidget {
+  const QuestionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
