39ffd181ca9e608350a4019db1e1cc1ecbe1489c
GitRockie <elromankos@gmail.com>
2023-01-09 15:14:45 +0100

    UI Control on TestOverview
    Painting the layout
    Passing the dynamic Object time from the controller with Obx()
    Constructin GridView.builder for allQuestions.length
    Returning allQestions with index, basic selection is working

--- lib/screens/question/test_overview_screen.dart
+++ lib/screens/question/test_overview_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/configs/themes/app_colors.dart';
+import 'package:flutter_application_1/configs/themes/custom_text_style.dart';
+import 'package:flutter_application_1/configs/themes/ui_parameters.dart';
 
 import 'package:flutter_application_1/controllers/question_paper/questions_controller.dart';
 import 'package:flutter_application_1/screens/home/question_card.dart';
 
 import 'package:flutter_application_1/widgets/common/background_decoration.dart';
 import 'package:flutter_application_1/widgets/common/custom_app_bar.dart';
+import 'package:flutter_application_1/widgets/content_area.dart';
+import 'package:flutter_application_1/widgets/questions/count_down_timer.dart';
 import 'package:get/get.dart';
 
 class TestOverviewScreen extends GetView<QuestionsController> {
@@ -14,14 +19,55 @@ class TestOverviewScreen extends GetView<QuestionsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: CustomAppBar(
-        title: controller.completedTest,
-      ),
-      body: const BackgrounDecoration(
-          child: Center(
-        child: Text('Test'),
-      )),
-    );
+        extendBodyBehindAppBar: true,
+        appBar: CustomAppBar(
+          title: controller.completedTest,
+        ),
+        body: BackgrounDecoration(
+          child: Column(
+            children: [
+              Expanded(
+                  child: ContentArea(
+                      child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CountDownTimer(
+                        color: UIParameters.isDarkMode()
+                            ? Theme.of(context).textTheme.bodyText1!.color
+                            : Theme.of(context).primaryColor,
+                        time: '',
+                      ),
+                      Obx(() => Text(
+                            '${controller.time} is remaining',
+                            style: countDownTimerStyle(),
+                          ))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: GridView.builder(
+                        shrinkWrap: true,
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: controller.allQuestions.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: Get.width ~/ 70,
+                            childAspectRatio: 1,
+                            crossAxisSpacing: 8,
+                            mainAxisSpacing: 8),
+                        itemBuilder: (_, index) {
+                          return Text(
+                            '${controller.allQuestions[index].selectedAnswer != null}',
+                            style: const TextStyle(fontSize: 20),
+                          );
+                        }),
+                  ),
+                ],
+              )))
+            ],
+          ),
+        ));
   }
 }
