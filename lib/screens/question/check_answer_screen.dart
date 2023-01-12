e994a9391890011aa5d94573af916ae814bf519e
GitRockie <elromankos@gmail.com>
2023-01-12 15:53:32 +0100

    Result Screen colored boxes
    GridView.builder add
    itemBuilder setting variables checking the _status
    return QuestionNumberCard depends on if/else condition
    Creating new Widget for onTap to check answer CheckAnswerScreen

---
+++ lib/screens/question/check_answer_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/controllers/question_paper/questions_controller.dart';
+import 'package:get/get.dart';
+
+class CheckAnswerScreen extends GetView<QuestionsController> {
+  const CheckAnswerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/question/result_screen.dart
+++ lib/screens/question/result_screen.dart
@@ -6,10 +6,12 @@ import 'package:flutter_application_1/controllers/question_paper/questions_contr
 import 'package:flutter_application_1/widgets/common/background_decoration.dart';
 import 'package:flutter_application_1/widgets/common/custom_app_bar.dart';
 import 'package:flutter_application_1/widgets/content_area.dart';
+import 'package:flutter_application_1/widgets/questions/answer_card.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
 import '../../controllers/question_paper/questions_controller.dart';
+import '../../widgets/questions/question_number_card.dart';
 
 class ResultScreen extends GetView<QuestionsController> {
   const ResultScreen({super.key});
@@ -57,6 +59,36 @@ class ResultScreen extends GetView<QuestionsController> {
             const SizedBox(
               height: 25,
             ),
+            Expanded(
+                child: GridView.builder(
+                    itemCount: controller.allQuestions.length,
+                    shrinkWrap: true,
+                    physics: const BouncingScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: Get.width ~/ 70,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                      childAspectRatio: 1,
+                    ),
+                    itemBuilder: (_, index) {
+                      final _question = controller.allQuestions[index];
+                      AnswerStatus _status = AnswerStatus.notAnswered;
+                      final _selectedAnswer = _question.selectedAnswer;
+                      final _correctAnswer = _question.correctAnswer;
+                      if (_selectedAnswer == _correctAnswer) {
+                        _status = AnswerStatus.correct;
+                      } else if (_question.selectedAnswer == null) {
+                        _status = AnswerStatus.incorrect;
+                      } else {
+                        _status = AnswerStatus.incorrect;
+                      }
+                      return QuestionNumberCard(
+                        index: index + 1,
+                        onTap: () =>
+                            controller.jumpToQuestion(index, goBack: false),
+                        status: _status,
+                      );
+                    }))
           ],
         )))
       ],
